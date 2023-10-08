7a4d029dd00fda536dd4752a7f4544bcc3363bad
Everton Russomanno <evertonmartins154@gmail.com>
2023-10-08 09:20:03 -0300

    Add findAll implementation

--- 07-projeto-aula276-jdbc/projeto-demo-dao-jdbc/src/application/Program.java
+++ 07-projeto-aula276-jdbc/projeto-demo-dao-jdbc/src/application/Program.java
@@ -13,7 +13,7 @@ public class Program {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println("=== TEST 1: seller findById =====");
+        System.out.println("\n=== TEST 1: seller findById =====");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
@@ -21,6 +21,12 @@ public class Program {
         Department department = new Department(2,null);
         List<Seller> list = sellerDao.findByDepartment(department);
         for(Seller obj : list){
+            System.out.println(obj);
+        }
+
+        System.out.println("\n=== TEST 3: seller findAll =====");
+        List<Seller> listAll = sellerDao.findAll();
+        for(Seller obj : listAll){
             System.out.println(obj);
         }
     }
