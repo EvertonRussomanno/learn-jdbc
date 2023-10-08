41e1f44475a302c1ef8fc1b0a3f2804819da5cc3
Everton Russomanno <evertonmartins154@gmail.com>
2023-10-08 07:10:16 -0300

    Add Department entity class

--- 07-projeto-aula276-jdbc/projeto-demo-dao-jdbc/src/application/Program.java
+++ 07-projeto-aula276-jdbc/projeto-demo-dao-jdbc/src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import model.entities.Department;
+
 public class Program {
     public static void main(String[] args) {
-
+        Department ogj = new Department(1, "Books");
+        System.out.println(ogj);
     }
 }
