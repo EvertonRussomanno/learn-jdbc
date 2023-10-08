9474dac9ce2f6f0333326ccb4b51ad0365af4925
Everton Russomanno <evertonmartins154@gmail.com>
2023-10-08 10:21:22 -0300

    Add delete implementation

--- 07-projeto-aula276-jdbc/projeto-demo-dao-jdbc/src/application/Program.java
+++ 07-projeto-aula276-jdbc/projeto-demo-dao-jdbc/src/application/Program.java
@@ -7,9 +7,12 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -40,5 +43,13 @@ public class Program {
         seller.setName("Martha Waine");
         sellerDao.update(seller);
         System.out.println("Update complete!");
+
+        System.out.println("\n=== TEST 6: seller delete =====");
+        System.out.print("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.delete(id);
+        System.out.println("Delete complete!");
+
+        sc.close();
     }
 }
